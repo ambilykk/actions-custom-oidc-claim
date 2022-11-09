930004453e9705f4a980661e41c591c9bafcd9ba
Ambily <ambilykk@Ambilys-MacBook-Pro.local>
2022-11-09 22:15:48 +0530

    bug fix

--- app/index.js
+++ app/index.js
@@ -10,17 +10,20 @@ const octokit = github.getOctokit(GITHUB_TOKEN);
 // inputs defined in action metadata file
 const owner = github.context.repository.owner;
 const repo = github.context.repository.repo;
-const url ='/repos/{owner}/{repo}/actions/oidc/customization/sub';
+const url = '/repos/{owner}/{repo}/actions/oidc/customization/sub';
 
+async function run() {
+    await octokit.request({
+        owner,
+        repo,
+        url,
+        method: 'PUT',
+        use_default: false,
+        include_claim_keys: [
+            'owner',
+            'repo'
+        ]
+    });
+}
 
-await octokit.request({
-    owner,
-    repo,
-    url,
-    method: 'PUT',
-    use_default: false,
-    include_claim_keys: [
-        'owner',
-        'repo'
-    ]
-  });
+run();
