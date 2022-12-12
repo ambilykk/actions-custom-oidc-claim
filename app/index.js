a3effe0282a988bceee8c34890564d9bfe4cb53d
Ambily <ambilykk@github.com>
2022-12-12 16:09:19 +0530

    commenting the display of request body

--- app/index.js
+++ app/index.js
@@ -47,7 +47,7 @@ async function run() {
              return;
         }
         
-        console.log(req_body);
+        // console.log(req_body);
         // call the github api to set the claim keys
         const response = await octokit.request(req_body);
         console.log(response);
